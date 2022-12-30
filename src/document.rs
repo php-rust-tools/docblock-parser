2baeb98bcca7a7fd4d7c831efb60c3fdf2d0933b
Ryan Chandler <ryangjchandler@gmail.com>
2022-12-30 00:10:54 +0000

    wip

--- src/document.rs
+++ src/document.rs
@@ -1,7 +1,30 @@
+#[derive(Debug)]
 pub struct Document {
     pub nodes: Vec<Node>,
 }
 
-pub enum Node {
+impl Document {
+    pub fn collect(&self) -> Vec<Node> {
+        self.nodes.to_vec()
+    }
+}
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum Node {
+    ParamTag {
+        tag: String,
+        _type: Option<Type>,
+        variable: Variable,
+        // description: Option<String>,
+    },
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Type {}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Variable {
+    pub name: String,
+    pub reference: bool,
+    pub variadic: bool,
 }
